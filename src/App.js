ea1995219e36d97f593da3f83154731f02b21bd7
demii21 <abhayarvind9@gmail.com>
2022-06-29 17:34:00 +0530

    text area

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-
-const { useEffect, useState } = React
-const CounterWithNameAndSideEffect = () => {
- const [count, setCount] = useState(0)
- useEffect(() => {
- console.log(`Component Method`)
- },[])
- return (
- <div>
- <p>You clicked {count} times</p>
- <button onClick={() => setCount(count + 1)}>Click me</button>
- </div>
- )
-}
-function WelcomeMessage({ myprop }) {
-  return <p>{myprop}</p>
- }
- 
-
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <CounterWithNameAndSideEffect/>
-        </p>
-        <WelcomeMessage myprop={'This is peepeepoopoo '} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar title="TextUtils"  />
+      <div className='container'>
+        <TextForm heading="Enter your text to analyze "/>
+      </div>
     </div>
   );
 }
